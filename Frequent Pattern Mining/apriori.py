74c99f33aa1770651cf83e3ab617119cba8dd8d0
Saif Mahmud Dhrubo <saif.dhrubo@gmail.com>
2019-08-22 17:42:26 +0600

    Apriori Completed - Trie Based
    Testing Required

--- Frequent Pattern Mining/apriori.py
+++ Frequent Pattern Mining/apriori.py
@@ -1,10 +1,13 @@
 import collections
 import itertools
 from trie_class import Trie
+import sys
+
 
 def load_dataset(filename):
 
-    dataset = [sorted(int(n) for n in i.strip().split()) for i in open(filename).readlines()]
+    dataset = [sorted(int(n) for n in i.strip().split())
+               for i in open(filename).readlines()]
     size = len(dataset)
 
     print('Size of the Dataset : ', size)
@@ -23,20 +26,22 @@ def load_dataset(filename):
 
 def find_frequent_1_itemsets(dataset, min_sup):
     min_sup = (len(dataset) * min_sup) // 100
-    frequency = dict(collections.Counter(itertools.chain.from_iterable(dataset)))
-    
+    frequency = dict(collections.Counter(
+        itertools.chain.from_iterable(dataset)))
+
     L1 = dict()
 
     for item, freq in frequency.items():
         if freq >= min_sup:
             L1[item] = freq
 
-
     # print(L1)
     return L1
 
 # Input : L_k (k : itemset size)
-def apriori_gen(L:list, k):
+
+
+def apriori_gen(L: list, k):
     # Self Join Step
     L_next = list()
 
@@ -47,9 +52,9 @@ def apriori_gen(L:list, k):
 
     # Removing Duplicates
     L_set = set(tuple(x) for x in L_next)
-    L_k1 = [ list(x) for x in L_set ]
+    L_k1 = [list(x) for x in L_set]
 
-    L_k1.sort(key = lambda x: L_next.index(x) )
+    L_k1.sort(key=lambda x: L_next.index(x))
     L_k1_tuple = [tuple(i) for i in L_k1]
 
     # Prune Step
@@ -61,7 +66,7 @@ def apriori_gen(L:list, k):
     return L_k1
 
 
-def has_infrequent_subset(candidate:tuple, L:list):
+def has_infrequent_subset(candidate: tuple, L: list):
     for subset in list(itertools.combinations(candidate, len(candidate) - 1)):
         if list(subset) not in L:
             return True
@@ -69,53 +74,62 @@ def has_infrequent_subset(candidate:tuple, L:list):
     return False
 
 
+def apriori(db: list, min_sup):
+    min_sup = (len(db) * min_sup) // 100
 
-def apriori(db:list, min_sup):
+    levels = list()
+
     L1 = find_frequent_1_itemsets(db, min_sup)
 
-    # print('L1', L1)
-    # pc : potential candidate
-    pc2 = list()
+    if bool(L1) == False:
+        print('No 1-Itemset Satisfies Given Minimum Support Threshold')
+        return None
+    
+    # Creating list of 1-itemset(list itself)
+    _L1 = list()
     for k in L1.keys():
         l = list()
         l.append(k)
 
-        pc2.append(l)
-    pc2 = sorted(pc2)
+        _L1.append(l)
+    _L1 = sorted(_L1)
+
+    # print('L1 :', L1)
+
+    levels.append(_L1)
+    # print('Levels :', levels)
+    
 
     while True:
-        c = apriori_gen(pc2, 1)
+        candidates = apriori_gen(levels[-1], len(levels[-1][0]))
+
         trie = Trie(db)
-        trie.build_trie(c)
+        trie.build_trie(candidates)
+        trie.assign_frequency()
 
         L = list()
 
-        for itemset in c:
+        for itemset in candidates:
             if trie.get_candidate_freq(itemset) >= min_sup:
                 L.append(itemset)
+                
+        if not L:
+            break
 
-        break
+        levels.append(L)
 
-    print(L)
-
-
-
-
-
-
-    # candidate2 = apriori_gen(sorted(), 1)
-
-    # print(candidate2)
-    # while True:
+    return levels
 
 
 if __name__ == "__main__":
-    db = load_dataset('Han.dat')
+    db = load_dataset(str(sys.argv[1]))
+    min_sup = float(sys.argv[2])
+    
+    print('Dataset :', str(sys.argv[1]))
+    print('Min Support :', min_sup, '%')
 
-    # L = [[1, 2], [1, 3], [1, 5], [2, 3], [2, 4], [2, 5]]
+    L = apriori(db, min_sup)
 
-    # L3 = apriori_gen(L, 2)
-    # print(L3)
-
-    apriori(db, (2/9) * 100)
-
+    if L is not None:
+        for i in range(len(L)):
+            print((i + 1), '- Frequent Itemsets :', L[i])
