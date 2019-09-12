3616b84220f00cc90fa82e16cec1e2044e55bf88
Tanjid Hasan Tonmoy <thTonmoy@users.noreply.github.com>
2019-09-13 01:12:38 +0600

    decision tree progress

--- Classification/DecisionTree.py
+++ Classification/DecisionTree.py
@@ -1,58 +1,71 @@
 import pandas as pd
-import numpy as np
+from pprint import pprint
+from attribute_selection import gain
+
 
 def load_datadet(filename:str):
     df = pd.read_csv(filename, header=None)
-
-    global dataset_size
-    dataset_size = df.shape[0]
-
     return df
 
 
-def entropy(dataset:pd.DataFrame, class_label_column):
-    entropy_node = 0
+class DecisionTree:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.decision_tree = None
+    
 
-    values = dataset[class_label_column].unique()
+    def fit(self,dataset:pd.DataFrame, class_label_column):
+        self.decision_tree = self._train_recursive(dataset, class_label_column)
+        
 
-    for val in values:
+    def _train_recursive(self,dataset:pd.DataFrame, class_label_column, tree=None):
+        # print(dataset.shape)
+        spl_attr = self._find_splitting_attribute(dataset, class_label_column)
+        attr_values = dataset[spl_attr].unique()
+        # print(spl_attr, attr_values)
 
-        # print(val, dataset[class_label_column].value_counts()[val])
+        if tree is None:
+            tree = {}
+            tree[spl_attr] = {}
 
-        p_i = dataset[class_label_column].value_counts()[val] / len(dataset[class_label_column])
-        entropy_node += (-p_i * np.log2(p_i))
-
-    return entropy_node
-
-
-def entropy_attribute(dataset:pd.DataFrame, class_label_column, attribute):
-    entropy_attr = 0
-
-    attr_vars = dataset[attribute].unique()
-
-    for val in attr_vars:
-        # print('\nAj =', val)
-
-        df_attr = dataset[dataset[attribute] == val]
-        # print(df_attr)
-
-        info = entropy(df_attr, class_label_column)
-        # print('Info(Dj) =', info)
-
-        # print('Dj :', df_attr.shape[0], 'D :', dataset_size)
-        fraction = df_attr.shape[0] / dataset_size
-        # print('Dj / D = ', fraction)
-
-        entropy_attr += (fraction * info)
-
-    return entropy_attr
+        for val in attr_values:
+            data_partition = dataset[dataset[spl_attr] == val]
+            data_partition = data_partition.drop(columns=[spl_attr]) #remove the splitting attribute
+            if data_partition.shape[0] == 0:
+                tree[spl_attr][val] =  'majority'
+                # print('empty')
+            class_values = data_partition[class_label_column].unique()
+            if len(class_values ) == 1:
+                tree[spl_attr][val] = class_values[0]
+                # print('pure class')
+            else:
+                #recursion
+                tree[spl_attr][val] = self._train_recursive(data_partition, class_label_column)
+        return tree
 
 
-def gain(dataset:pd.DataFrame, class_label_column, attribute):
 
-    _gain = entropy(dataset, class_label_column) - entropy_attribute(dataset, class_label_column, attribute)
+    def _find_splitting_attribute(self, dataset:pd.DataFrame, class_label_column):
+        attr_cols = list(dataset.columns)
+        attr_cols.remove(class_label_column)
+        # print(attr_cols)
+        max_gain = 0
+        splitting_attr = None
+        for col in attr_cols:
+            g = gain(data, class_label_column, col)
+            if g > max_gain:
+                splitting_attr = col
+                max_gain = max(g, max_gain)
+        # print(max_gain, splitting_attr)
+        return splitting_attr
 
-    return _gain
+    
+    def predict(x):
+        pass
+
+
+    def print_tree(self):
+        pprint(self.decision_tree)
 
 
 if __name__ == '__main__':
@@ -61,6 +74,8 @@ if __name__ == '__main__':
     # info = entropy(data, 0)
     # print(info)
 
-    g = gain(data, 0, 9)
-    print(g)
+    dt = DecisionTree()
+
+    tree = dt.fit(data, class_label_column=0)
+    dt.print_tree()
 
